3126015b6a17df2266294cd8e90ffbed0a56529b
zhang <xyzhang0817@gmail.com>
2016-04-16 14:09:03 -0400

    Trying onConnected function implement.

--- app/src/main/java/com/example/zhang/location/MainActivity.java
+++ app/src/main/java/com/example/zhang/location/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.zhang.location;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity
         GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 5000;
     private TextView txtOutput;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
@@ -53,6 +56,23 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // Update location every second.
+
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+//                PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+//                    MY_PERMISSION_ACCESS_FINE_LOCATION);
+//        }
+
+        if (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this
+        );
     }
 
     @Override
