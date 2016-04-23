6e9af971a058250225498ca1e61f9cbc9819d1b2
zhang <xyzhang0817@gmail.com>
2016-04-23 16:26:43 -0400

    Resetting the app.

--- app/src/main/java/com/example/zhang/location/MainActivity.java
+++ app/src/main/java/com/example/zhang/location/MainActivity.java
@@ -1,203 +1,35 @@
 package com.example.zhang.location;
 
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.ActivityRecognition;
-import com.google.android.gms.location.DetectedActivity;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity
-        implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        ResultCallback<Status>{
-
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    private ActivityDetectionBroadcastReceiver mActivityDetectionBroadcastReceiver;
-    private GoogleApiClient mGoogleApiClient;
-
-    private TextView mStatusTextView;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mStatusTextView = (TextView) findViewById(R.id.detectedActivities);
-
-        mActivityDetectionBroadcastReceiver = new ActivityDetectionBroadcastReceiver();
-
-        buildGoogleApiClient();
-
-    }
-
-    private synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(ActivityRecognition.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // Connect the Client.
-        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mActivityDetectionBroadcastReceiver);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        LocalBroadcastManager.getInstance(this).registerReceiver(mActivityDetectionBroadcastReceiver,
-                new IntentFilter(Constants.BROADCAST_ACTION));
     }
 
     @Override
     protected void onStop() {
-        // Disconnect the Client.
-        mGoogleApiClient.disconnect();
-
         super.onStop();
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
 
-        Log.i(LOG_TAG, "Connected to GoogleApiClient");
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        // attempt to re-establish the connection.
-        Log.i(LOG_TAG, "GoogleApiClient on connection suspended.");
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-        Log.i(LOG_TAG, "GoogleApiClient on connection failed: " + connectionResult.getErrorCode());
-    }
-
-    @Override
-    public void onResult(@NonNull Status status) {
-
-        if (status.isSuccess()) {
-            Log.e(LOG_TAG, "Successfully added activity detection.");
-
-        } else {
-            Log.e(LOG_TAG, "Error adding or removing activity detection: " + status.getStatusMessage());
-        }
-    }
-
-    public void requestActivityUpdatesButtonHandler (View view) {
-        if (!mGoogleApiClient.isConnected()) {
-            Toast.makeText(this, getString(R.string.not_connected),
-                    Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(
-                mGoogleApiClient,
-                Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
-                getActivityDetectionPendingIntent()
-        ).setResultCallback(this);
-
-        mStatusTextView.setText(R.string.request_activity_updates);
-    }
-
-    public void removeActivityUpdatesButtonHandler(View view) {
-        if (!mGoogleApiClient.isConnected()) {
-            Toast.makeText(this, getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
-            return;
-        }
-        // Remove all activity updates for the PendingIntent that was used to request activity
-        // updates.
-        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(
-                mGoogleApiClient,
-                getActivityDetectionPendingIntent()
-        ).setResultCallback(this);
-
-    }
-
-    public PendingIntent getActivityDetectionPendingIntent() {
-        Intent intent = new Intent(this, DetectedActivitiesIntentService.class);
-        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
-
-    /**
-     * Returns a human readable String corresponding to a detected activity type.
-     */
-    public String getActivityString(int detectedActivityType) {
-        Resources resources = this.getResources();
-        switch(detectedActivityType) {
-            case DetectedActivity.IN_VEHICLE:
-                return resources.getString(R.string.in_vehicle);
-            case DetectedActivity.ON_BICYCLE:
-                return resources.getString(R.string.on_bicycle);
-            case DetectedActivity.ON_FOOT:
-                return resources.getString(R.string.on_foot);
-            case DetectedActivity.RUNNING:
-                return resources.getString(R.string.running);
-            case DetectedActivity.STILL:
-                return resources.getString(R.string.still);
-            case DetectedActivity.TILTING:
-                return resources.getString(R.string.tilting);
-            case DetectedActivity.UNKNOWN:
-                return resources.getString(R.string.unknown);
-            case DetectedActivity.WALKING:
-                return resources.getString(R.string.walking);
-            default:
-                return resources.getString(R.string.unidentifiable_activity, detectedActivityType);
-        }
-    }
-
-
-    public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver {
-
-        protected final String LOG_TAG = ActivityDetectionBroadcastReceiver.class.getSimpleName();
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            ArrayList<DetectedActivity> updateActivities = intent.getParcelableArrayListExtra(Constants.ACTIVITY_EXTRA);
-
-            String strStatus = "";
-            for (DetectedActivity thisActivity : updateActivities) {
-                strStatus += getActivityString(thisActivity.getType()) + thisActivity.getConfidence() + "%\n";
-            }
-
-            mStatusTextView.setText(strStatus);
-        }
-    }
 }
