d006283b5a17038f8862ac09d7e310bdb90a53f9
zhang <xyzhang0817@gmail.com>
2016-04-24 15:34:32 -0400

    Getting geofencing event.

---
+++ app/src/main/java/com/example/zhang/location/GeofenceTransitionsIntentService.java
@@ -0,0 +1,36 @@
+package com.example.zhang.location;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.GeofencingEvent;
+
+/**
+ * Created by zhang on 24/04/16.
+ */
+public class GeofenceTransitionsIntentService extends IntentService {
+
+    private static final String LOG_TAG = GeofenceTransitionsIntentService.class.getSimpleName();
+
+    public GeofenceTransitionsIntentService() {
+        super(LOG_TAG);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        if (geofencingEvent.hasError()) {
+            String errorMessage = GeofencingErrorMessages.getErrorString(this,
+                    geofencingEvent.getErrorCode());
+            Log.e(LOG_TAG, errorMessage);
+            return;
+        }
+    }
+}
